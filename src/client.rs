32425e415e5b22952d230e0ecda273274a7eb8ea
Endre Karlson <endre.karlson@hp.com>
2014-12-03 09:26:34 +0100

    Make client take a type + name arg

--- src/client.rs
+++ src/client.rs
@@ -1,29 +1,39 @@
 extern crate dns;
+extern crate docopt;
+extern crate serialize;
 
 use std::default;
-use std::str::from_str;
 
-use std::io::BufWriter;
-use std::io::net::ip::{IpAddr,Port,SocketAddr};
-use std::io::net::udp::UdpSocket;
+use docopt::Docopt;
 
 use dns::client::Client;
-use dns::client::config::ClientConfig;
 
-use dns::io::DNSWriter;
 use dns::message::{Message, Header};
 use dns::query::Question;
 use dns::types::Type;
 use dns::utils;
 
+static USAGE: &'static str = "
+Usage: rhost <rtype> <name>
+";
+
+#[deriving(Decodable, Show)]
+struct Args {
+    arg_rtype: String,
+    arg_name: String
+}
+
+
 fn main() {
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
+
     let header = Header {..default::Default::default() };
 
     let mut questions: Vec<Question> = Vec::new();
 
     let q: Question = Question {
-        name: utils::name_from_slice("google.com"),
-        ty: Type::A,
+        name: utils::name_from_slice(args.arg_name.as_slice()),
+        ty: from_str::<Type>(args.arg_rtype.as_slice()).unwrap(),
         ..default::Default::default()
     };
 
@@ -35,9 +45,9 @@ fn main() {
         ..default::Default::default()
     };
 
-
     let client: Client = Client::new();
-    client.exchange(&mut msg);
 
-    println!("Msg {}", msg);
+    println!("About to query {} {} towards servers {}", args.arg_rtype, args.arg_name, client.config.servers)
+
+    let msg = client.exchange(&mut msg).unwrap();
 }
